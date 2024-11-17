135e347cf144fb253d5f9ab7d5603372219405a0
Juan Pablo <juan.pablo.arias.0204@gmail.com>
2024-11-17 18:00:02 -0500

    creando fallas a proposito

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -5,6 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'  # Cambia esto por una clave segura en producción
 
+#mi vulnerabilidad
 DEBUG = True
 
 ALLOWED_HOSTS = []
