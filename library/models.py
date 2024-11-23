4867938047cc675bed71c3a1d0c33b104157cc1c
darkcode0 <tupabloarias12345@gmail.com>
2024-11-22 23:32:59 -0500

    cambios realizados para pruebas

--- library/models.py
+++ library/models.py
@@ -15,5 +15,5 @@ class Book(models.Model):
             'author': self.author,
             'year': self.year,
         }
-
-#realizando cambios en el modelo
+https://dev.azure.com/Unisabaneta/Sag Academico/_wiki/wikis/Sag-Academico.wiki/10/Pipelines-para-diferentes-tecnologias
+#realizando cambios en el modelo wdnwd
