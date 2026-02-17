d1fb383d977884d13460209023d2b1163e789c62
dark <juan.pablo.arias.0204@gmail.com>
2026-02-16 23:20:54 -0500

    mis aportes

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'your-secret-key'  # Cambia esto por una clave segura en producción
+SECRET_KEY = 'llave-secreta'  # Cambia esto por una clave segura en producción
 
 #mi vulnerabilidad
 DEBUG = True
